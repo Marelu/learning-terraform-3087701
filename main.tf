d54ddaf84b3090b4800af74478bbd3ff2aaa33cf
Marelu <mariusjucan@gmail.com>
2023-05-04 13:38:36 +0300

    Cheat sheet

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ data "aws_ami" "app_ami" {
 #  default = true
 #}
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  #version = "3.14.2"
 
   name = "dev"
   cidr = "10.0.0.0/16"
@@ -40,6 +40,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -53,7 +54,8 @@ module "blog_sg" {
   name = "blog"
 
   #vpc_id              = data.aws_vpc.default.id
-  vpc_id              = module.vpc.public_subnets[0]
+  #vpc_id              = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
